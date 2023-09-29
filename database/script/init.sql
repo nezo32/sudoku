c954df7251c461f285d24692f1dfe9923f3c63fd
nezo32 <nezo.prod32@mail.ru>
2023-09-29 17:12:38 +0300

    feat(database): docker compose change & basic init sql implement
    add postgres & pgadmin image to docker compose, implement initial sql file with creation of tables users, games

---
+++ database/script/init.sql
@@ -0,0 +1,17 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS users (
+    id uuid DEFAULT uuid_generate_v4(),
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT user_username CHECK(username != '')
+);
+CREATE TABLE IF NOT EXISTS games (
+    id uuid DEFAULT uuid_generate_v4(),
+    player_id uuid,
+    board CHAR [] [] NOT NULL,
+    begin_date date,
+    PRIMARY KEY (id)
+);
+ALTER TABLE games
+ADD FOREIGN KEY (player_id) REFERENCES users(id)
