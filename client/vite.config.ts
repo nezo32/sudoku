c97592adca65c4592d2089a780b9d2d520a35bec
nezo32 <nezo.prod32@mail.ru>
2023-09-29 12:23:02 +0300

    chore(client): add vite vuetify plugin & env pass

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return {
+    plugins: [vue(), vuetify({ autoImport: true })],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
