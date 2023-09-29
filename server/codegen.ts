a0581213676230f0941d3222dd6a0e051df072b9
nezo32 <nezo.prod32@mail.ru>
2023-09-30 00:39:22 +0300

    build(server): add dependencies, change graphql codegen & tsconfig
    tsconfig add absolute path; add prisma, jwt, dotenv, bcrypt, gql-scalars, module-alias, prettier packages; set up module alias package; prisma codegen scripts implement

--- server/codegen.ts
+++ server/codegen.ts
@@ -2,9 +2,13 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 import { defineConfig } from "@eddeee888/gcg-typescript-resolver-files";
 
 const config: CodegenConfig = {
-  schema: "**/schema.graphql",
+  schema: "**/.graphql",
   generates: {
     "src/schema": defineConfig(),
+  },
+  emitLegacyCommonJSImports: false,
+  hooks: {
+    afterOneFileWrite: ["prettier --write"],
   },
 };
 export default config;
