fcb06873fc6d09e66827041be2ec142fc485b0ba
nezo32 <nezo.prod32@mail.ru>
2023-09-30 18:26:08 +0300

    feat(server): login mutation fully implemented (need to overthing about cookie domain)

--- server/src/schema/types.generated.ts
+++ server/src/schema/types.generated.ts
@@ -1,89 +1,85 @@
-import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from "graphql";
+import { RequestContextType } from "src/context/index";
 export type Maybe<T> = T | null | undefined;
 export type InputMaybe<T> = T | null | undefined;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never };
 export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
-  DateTime: { input: Date | string; output: Date | string; }
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+  DateTime: { input: Date | string; output: Date | string };
 };
 
 export type AuthData = {
-  password: Scalars['String']['input'];
-  username: Scalars['String']['input'];
+  password: Scalars["String"]["input"];
+  username: Scalars["String"]["input"];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  login?: Maybe<Scalars['Boolean']['output']>;
-  register?: Maybe<Scalars['Boolean']['output']>;
+  __typename?: "Mutation";
+  login?: Maybe<Scalars["Boolean"]["output"]>;
+  register?: Maybe<Scalars["Boolean"]["output"]>;
 };
-
 
 export type MutationloginArgs = {
   data: AuthData;
 };
-
 
 export type MutationregisterArgs = {
   data: AuthData;
 };
 
 export type Query = {
-  __typename?: 'Query';
-  getNewSudoku: Array<Array<Maybe<Scalars['String']['output']>>>;
-  validateSudoku?: Maybe<Scalars['Boolean']['output']>;
+  __typename?: "Query";
+  getNewSudoku: Array<Array<Maybe<Scalars["String"]["output"]>>>;
+  validateSudoku?: Maybe<Scalars["Boolean"]["output"]>;
 };
-
 
 export type QuerygetNewSudokuArgs = {
-  countOfBeginNumbers?: InputMaybe<Scalars['Int']['input']>;
-  size?: InputMaybe<Scalars['Int']['input']>;
+  countOfBeginNumbers?: InputMaybe<Scalars["Int"]["input"]>;
+  size?: InputMaybe<Scalars["Int"]["input"]>;
 };
-
 
 export type QueryvalidateSudokuArgs = {
-  sudoku: Array<Array<InputMaybe<Scalars['String']['input']>>>;
+  sudoku: Array<Array<InputMaybe<Scalars["String"]["input"]>>>;
 };
 
-
-
 export type ResolverTypeWrapper<T> = Promise<T> | T;
-
 
 export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
   resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
-export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => Promise<TResult> | TResult;
 
 export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => TResult | Promise<TResult>;
 
 export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
@@ -107,10 +103,14 @@ export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TCo
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
   parent: TParent,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
+  obj: T,
+  context: TContext,
+  info: GraphQLResolveInfo,
+) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
@@ -119,50 +119,68 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => TResult | Promise<TResult>;
-
-
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
   AuthData: AuthData;
-  String: ResolverTypeWrapper<Scalars['String']['output']>;
-  DateTime: ResolverTypeWrapper<Scalars['DateTime']['output']>;
+  String: ResolverTypeWrapper<Scalars["String"]["output"]>;
+  DateTime: ResolverTypeWrapper<Scalars["DateTime"]["output"]>;
   Mutation: ResolverTypeWrapper<{}>;
-  Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
+  Boolean: ResolverTypeWrapper<Scalars["Boolean"]["output"]>;
   Query: ResolverTypeWrapper<{}>;
-  Int: ResolverTypeWrapper<Scalars['Int']['output']>;
+  Int: ResolverTypeWrapper<Scalars["Int"]["output"]>;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
   AuthData: AuthData;
-  String: Scalars['String']['output'];
-  DateTime: Scalars['DateTime']['output'];
+  String: Scalars["String"]["output"];
+  DateTime: Scalars["DateTime"]["output"];
   Mutation: {};
-  Boolean: Scalars['Boolean']['output'];
+  Boolean: Scalars["Boolean"]["output"];
   Query: {};
-  Int: Scalars['Int']['output'];
+  Int: Scalars["Int"]["output"];
 };
 
-export interface DateTimeScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['DateTime'], any> {
-  name: 'DateTime';
+export interface DateTimeScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes["DateTime"], any> {
+  name: "DateTime";
 }
 
-export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
-  login?: Resolver<Maybe<ResolversTypes['Boolean']>, ParentType, ContextType, RequireFields<MutationloginArgs, 'data'>>;
-  register?: Resolver<Maybe<ResolversTypes['Boolean']>, ParentType, ContextType, RequireFields<MutationregisterArgs, 'data'>>;
+export type MutationResolvers<
+  ContextType = RequestContextType,
+  ParentType extends ResolversParentTypes["Mutation"] = ResolversParentTypes["Mutation"],
+> = {
+  login?: Resolver<Maybe<ResolversTypes["Boolean"]>, ParentType, ContextType, RequireFields<MutationloginArgs, "data">>;
+  register?: Resolver<
+    Maybe<ResolversTypes["Boolean"]>,
+    ParentType,
+    ContextType,
+    RequireFields<MutationregisterArgs, "data">
+  >;
 };
 
-export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
-  getNewSudoku?: Resolver<Array<Array<Maybe<ResolversTypes['String']>>>, ParentType, ContextType, RequireFields<QuerygetNewSudokuArgs, 'countOfBeginNumbers' | 'size'>>;
-  validateSudoku?: Resolver<Maybe<ResolversTypes['Boolean']>, ParentType, ContextType, RequireFields<QueryvalidateSudokuArgs, 'sudoku'>>;
+export type QueryResolvers<
+  ContextType = RequestContextType,
+  ParentType extends ResolversParentTypes["Query"] = ResolversParentTypes["Query"],
+> = {
+  getNewSudoku?: Resolver<
+    Array<Array<Maybe<ResolversTypes["String"]>>>,
+    ParentType,
+    ContextType,
+    RequireFields<QuerygetNewSudokuArgs, "countOfBeginNumbers" | "size">
+  >;
+  validateSudoku?: Resolver<
+    Maybe<ResolversTypes["Boolean"]>,
+    ParentType,
+    ContextType,
+    RequireFields<QueryvalidateSudokuArgs, "sudoku">
+  >;
 };
 
-export type Resolvers<ContextType = any> = {
+export type Resolvers<ContextType = RequestContextType> = {
   DateTime?: GraphQLScalarType;
   Mutation?: MutationResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
 };
-
