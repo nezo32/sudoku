11cdc38a0d3072bec0499f5014f0b8bfab4820cc
nezo32 <nezo.prod32@mail.ru>
2023-09-30 18:24:52 +0300

    feat(server): add getUser func to get user by username, prisma client databaseURL get from file

--- server/src/db/user.ts
+++ server/src/db/user.ts
@@ -1,10 +1,9 @@
 import { prisma } from "@/prisma";
 
-export async function getUser(username: string, password: string) {
+export async function getUser(username: string) {
   return await prisma.users.findFirst({
     where: {
       username,
-      password,
     },
   });
 }
